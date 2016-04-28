11162116bbcd2695d7bf20e834dab6981e063c40
senth <senth.wallace@gmail.com>
2016-04-28 15:51:48 +0200

    Fix: Buttons are now colored correctly in API 20 & 21

--- app-utils/src/main/java/com/spiddekauga/android/ButtonHelper.java
+++ app-utils/src/main/java/com/spiddekauga/android/ButtonHelper.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.res.ColorStateList;
 import android.os.Build;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.AppCompatButton;
 import android.widget.Button;
 
 /**
@@ -11,16 +12,21 @@ import android.widget.Button;
  */
 public class ButtonHelper {
 /**
- * Set the color of the button (only effects API v22 or older)
+ * Set the color of the button (only effects API v20 or before)
  * @param button the button to set the color of
  * @param color the color of the button
  */
-@TargetApi(22)
-@SuppressWarnings("deprecated")
+@TargetApi(21)
+@SuppressWarnings("deprecation")
 public static void setColor(Button button, ButtonColors color) {
-	if (Build.VERSION.SDK_INT <= 22) {
+	if (Build.VERSION.SDK_INT <= 21) {
 		ColorStateList colors = AppActivity.getActivity().getResources().getColorStateList(color.getColorResId());
-		ViewCompat.setBackgroundTintList(button, colors);
+
+		if (Build.VERSION.SDK_INT >= 20 && button instanceof AppCompatButton) {
+			((AppCompatButton) button).setSupportBackgroundTintList(colors);
+		} else {
+			ViewCompat.setBackgroundTintList(button, colors);
+		}
 	}
 }
 }
