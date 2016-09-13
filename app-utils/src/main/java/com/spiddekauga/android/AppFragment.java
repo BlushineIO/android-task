96f84192e271a2447a1aa398e7cd0d6b7a64d500
senth <senth.wallace@gmail.com>
2016-09-13 09:53:24 +0200

    Add gotoFragment() for easy access going back/to a fragment

--- app-utils/src/main/java/com/spiddekauga/android/AppFragment.java
+++ app-utils/src/main/java/com/spiddekauga/android/AppFragment.java
@@ -2,7 +2,6 @@ package com.spiddekauga.android;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
 import android.support.annotation.Nullable;
@@ -10,12 +9,15 @@ import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import com.spiddekauga.utils.EventBus;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import de.mrapp.android.dialog.MaterialDialog;
@@ -26,12 +28,57 @@ import de.mrapp.android.dialog.MaterialDialog;
 public abstract class AppFragment extends Fragment {
 private static final String TAG = AppFragment.class.getSimpleName();
 private static final EventBus mEventBus = EventBus.getInstance();
-private static Typeface mToolbarFont = null;
 @StringRes
 int mBackMessage;
 @StringRes
 int mBackPositiveActionText = R.string.discard;
 private AppFragmentHelper mFragmentHelper = new AppFragmentHelper();
+
+/**
+ * Go to the specified fragment. If the fragment exists in the back stack it will pop to this
+ * fragment. Otherwise it will create a new fragment
+ * @param fragmentClass the fragment class to goto to (or create)
+ */
+public static void gotoFragment(Class<? extends AppFragment> fragmentClass) {
+	if (existsInBackStack(fragmentClass)) {
+		AppActivity.getActivity().getSupportFragmentManager().popBackStackImmediate(fragmentClass.getSimpleName(), 0);
+	} else {
+		try {
+			Constructor<? extends AppFragment> constructor = fragmentClass.getConstructor();
+			AppFragment appFragment = constructor.newInstance();
+			appFragment.show();
+		} catch (NoSuchMethodException | IllegalAccessException | java.lang.InstantiationException | InvocationTargetException e) {
+			Log.e(TAG, "newFirstScreenInstance() - Failed to find Constructor", e);
+		}
+	}
+}
+
+/**
+ * Checks if a fragment class exists in the back stack
+ * @param fragmentClass the fragment class to check if it exists in the back stack
+ * @return true if the fragmentClass exists in the back stack
+ */
+public static boolean existsInBackStack(Class<? extends Fragment> fragmentClass) {
+	FragmentManager fragmentManager = AppActivity.getActivity().getSupportFragmentManager();
+	for (int i = 0; i < fragmentManager.getBackStackEntryCount(); i++) {
+		FragmentManager.BackStackEntry backStackEntry = fragmentManager.getBackStackEntryAt(0);
+		if (fragmentClass.getSimpleName().equals(backStackEntry.getName())) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * Show the current fragment
+ */
+public void show() {
+	AppActivity activity = AppActivity.getActivity();
+	FragmentTransaction fragmentTransaction = activity.getSupportFragmentManager().beginTransaction();
+	fragmentTransaction.replace(android.R.id.content, this);
+	fragmentTransaction.addToBackStack(getClass().getSimpleName());
+	fragmentTransaction.commit();
+}
 
 static Fragment getVisibleFragment() {
 	FragmentManager fragmentManager = AppActivity.getActivity().getSupportFragmentManager();
@@ -146,33 +193,6 @@ public void dismiss() {
 	} else {
 		AppActivity.getActivity().supportFinishAfterTransition();
 	}
-}
-
-/**
- * Show the current fragment
- */
-public void show() {
-	AppActivity activity = AppActivity.getActivity();
-	FragmentTransaction fragmentTransaction = activity.getSupportFragmentManager().beginTransaction();
-	fragmentTransaction.replace(android.R.id.content, this);
-	fragmentTransaction.addToBackStack(getClass().getSimpleName());
-	fragmentTransaction.commit();
-}
-
-/**
- * Checks if a fragment class exists in the back stack
- * @param fragmentClass the fragment class to check if it exists in the back stack
- * @return true if the fragmentClass exists in the back stack
- */
-protected boolean existsInBackStack(Class<? extends Fragment> fragmentClass) {
-	FragmentManager fragmentManager = AppActivity.getActivity().getSupportFragmentManager();
-	for (int i = 0; i < fragmentManager.getBackStackEntryCount(); i++) {
-		FragmentManager.BackStackEntry backStackEntry = fragmentManager.getBackStackEntryAt(0);
-		if (fragmentClass.getSimpleName().equals(backStackEntry.getName())) {
-			return true;
-		}
-	}
-	return false;
 }
 
 }
