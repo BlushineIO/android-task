e4c1b487126c9fb1eeb787242ad8ce6df1ed0d5b
senth <senth.wallace@gmail.com>
2018-01-31 16:50:58 +0100

    Fix opening InfoActivity and InfoFragment

--- app-utils/src/main/java/com/spiddekauga/android/Fragment.java
+++ app-utils/src/main/java/com/spiddekauga/android/Fragment.java
@@ -43,6 +43,9 @@ protected void declareArgument(String argumentName, AppFragment.ArgumentRequired
 @Nullable
 @Override
 public final View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+	fetchArguments();
+	onArgumentsSet();
+	
 	if (mView == null) {
 		mView = onCreateViewImpl(inflater, container, savedInstanceState);
 		mCreatedNewView = true;
@@ -53,24 +56,6 @@ public final View onCreateView(LayoutInflater inflater, @Nullable ViewGroup cont
 	return mView;
 }
 
-/**
- * Called to have the fragment instantiate its user interface view.
- * This is optional, and non-graphical fragments can return null (which
- * is the default implementation).  This will be called between
- * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
- * <p>
- * <p>If you return a View from here, you will later be called in
- * {@link #onDestroyView} when the view is being released.
- * @param inflater The LayoutInflater object that can be used to inflate any views in the fragment,
- * @param container If non-null, this is the parent view that the fragment's UI should be attached
- * to.  The fragment should not add the view itself, but this can be used to generate the
- * LayoutParams of the view.
- * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous
- * saved state as given here.
- * @return Return the View for the fragment's UI, or null.
- */
-public abstract View onCreateViewImpl(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState);
-
 @Override
 public final void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 	super.onViewCreated(view, savedInstanceState);
@@ -80,11 +65,17 @@ public final void onViewCreated(View view, @Nullable Bundle savedInstanceState) 
 	}
 }
 
-@Override
-public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-	super.onActivityCreated(savedInstanceState);
-	fetchArguments();
-	onArgumentsSet();
+/**
+ * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
+ * has returned, but before any saved state has been restored in to the view.
+ * This gives subclasses a chance to initialize themselves once
+ * they know their view hierarchy has been completely created.  The fragment's
+ * view hierarchy is not however attached to its parent at this point.
+ * @param view The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+ * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous
+ * saved state as given here.
+ */
+public void onViewCreatedImpl(View view, @Nullable Bundle savedInstanceState) {
 }
 
 /**
@@ -114,17 +105,22 @@ protected void onArgumentsSet() {
 }
 
 /**
- * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
- * has returned, but before any saved state has been restored in to the view.
- * This gives subclasses a chance to initialize themselves once
- * they know their view hierarchy has been completely created.  The fragment's
- * view hierarchy is not however attached to its parent at this point.
- * @param view The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+ * Called to have the fragment instantiate its user interface view.
+ * This is optional, and non-graphical fragments can return null (which
+ * is the default implementation).  This will be called between
+ * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+ * <p>
+ * <p>If you return a View from here, you will later be called in
+ * {@link #onDestroyView} when the view is being released.
+ * @param inflater The LayoutInflater object that can be used to inflate any views in the fragment,
+ * @param container If non-null, this is the parent view that the fragment's UI should be attached
+ * to.  The fragment should not add the view itself, but this can be used to generate the
+ * LayoutParams of the view.
  * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous
  * saved state as given here.
+ * @return Return the View for the fragment's UI, or null.
  */
-public void onViewCreatedImpl(View view, @Nullable Bundle savedInstanceState) {
-}
+public abstract View onCreateViewImpl(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState);
 
 /**
  * Get the argument value
