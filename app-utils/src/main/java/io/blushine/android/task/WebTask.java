17d4d765681d85126028e7e495bfa5f0ab39f1bd
senth <senth.wallace@gmail.com>
2018-05-07 11:34:05 +0200

    Add check for NETWORK_STATE permission

--- app-utils/src/main/java/io/blushine/android/task/WebTask.java
+++ app-utils/src/main/java/io/blushine/android/task/WebTask.java
@@ -1,8 +1,11 @@
 package io.blushine.android.task;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.ContextCompat;
 
 import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
@@ -23,7 +26,7 @@ import okhttp3.Response;
  * Base class for all tasks requiring an Internet connection
  */
 public abstract class WebTask<Params, Progress, Event extends ResponseEvent> extends EventTask<Params, Progress, Event> {
-protected static final int RETRIES = 5;
+private static final int RETRIES = 5;
 private static final long SLEEP_TIME = 10 * 1000; // IN MS
 private static OkHttpClient mOkHttpClient = new OkHttpClient();
 private WaitTypes mWaitType;
@@ -34,11 +37,11 @@ private WaitTypes mWaitType;
  */
 protected WebTask(WaitTypes waitType) {
 	mWaitType = waitType;
-
+	
 	CookieHandler cookieHandler = new CookieManager(null, CookiePolicy.ACCEPT_ALL);
 	CookieHandler.setDefault(cookieHandler);
 	CookieJar cookieJar = new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(AppActivity.getActivity()));
-
+	
 	mOkHttpClient = new OkHttpClient.Builder()
 			.cookieJar(cookieJar)
 			.build();
@@ -65,42 +68,42 @@ protected void setWaitType(WaitTypes waitType) {
 @SafeVarargs
 protected final Event doInBackground(Params... params) {
 	Event event = newEvent(params);
-
+	
 	switch (mWaitType) {
 	case WAIT_CONNECTION:
 		waitForConnection();
 		break;
-
+	
 	case WAIT_USER_LOGGED_IN:
 		waitForUserLoggedIn();
 		break;
-
+	
 	case CHECK_CONNECTION:
 		if (!isConnected()) {
 			event.setStatus(ResponseEvent.ResponseStatuses.FAILED_NO_INTERNET);
 			return event;
 		}
 		break;
-
+	
 	case CHECK_USER_LOGGED_IN:
 		if (!isUserLoggedIn()) {
 			event.setStatus(ResponseEvent.ResponseStatuses.FAILED_USER_LOGGED_OUT);
 			return event;
 		}
 		break;
-
+	
 	case ONLY_RETRY:
 		// Does nothing
 		break;
 	}
-
+	
 	// Retry
 	int retriesLeft = RETRIES;
 	while (retriesLeft > 0 && !event.isSuccessful()) {
 		doInBackground(event, params);
 		retriesLeft -= 1;
 	}
-
+	
 	return event;
 }
 
@@ -126,8 +129,12 @@ protected abstract void waitForUserLoggedIn();
  */
 protected boolean isConnected() {
 	ConnectivityManager connectivityManager = (ConnectivityManager) AppActivity.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-	NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-	return networkInfo != null && networkInfo.isConnected();
+	if (ContextCompat.checkSelfPermission(AppActivity.getActivity(), Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_GRANTED) {
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		return networkInfo != null && networkInfo.isConnected();
+	} else {
+		return true;
+	}
 }
 
 /**
